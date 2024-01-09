555079aff7ea46def549aa936bf8c44124ac5605
Tiberius <35663392+TIberiu99p@users.noreply.github.com>
2024-01-09 14:16:27 +0000

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -5,6 +5,32 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName 
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value; 
+            }  
+        }
 
     }
 }
